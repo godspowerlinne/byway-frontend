eed8a3b8a24a3ea0dd8e8b18c01891e9c4d856d2
godspowerlinne <amriccygodspea@gmail.com>
2025-03-02 15:28:09 +0100

    feat: Implement global theme context
    - a new Theme component that provides a global context for managing theme preferences (light/dark). The user's preferred theme is persisted in localStorage, ensuring a consistent experience across sessions.

---
+++ src/base/Theme.jsx
@@ -0,0 +1,39 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+// Implement a global context for accessing core functionalities from any component
+const ThemeContext = createContext();
+
+const Theme = ({ children }) => {
+        // step 1: set state to store theme (light / dark).
+    const [isDarkMode, setIsDarkMode] = useState(()=>{
+        return localStorage.getItem("theme") === "dark";
+    });
+    
+    const toggleTheme = () =>{
+        // step 2: Set function to change theme
+        setIsDarkMode((prevMode)=>{
+            const newTheme = !prevMode;
+            localStorage.setItem("theme", newTheme ? "dark" : "light");
+            return newTheme;
+        });
+        // step 3 apply the theme to html tag
+        useEffect(()=>{
+            document.documentElement.className = isDarkMode ? "dark" : "light";
+        }, [isDarkMode]);
+    }
+    return (
+        <ThemeContext.Provider value={{isDarkMode, toggleTheme }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export default Theme;
+
+export const useTheme = () => {
+    const context = useContext(ThemeContext);
+    if (!context) {
+        throw new Error("`useTheme` must be used within a `useTheme`");
+    }
+    return context;
+};
