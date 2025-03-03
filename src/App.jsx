afca95c7aeffe9703029535766da0f96184a5761
godspowerlinne <amriccygodspea@gmail.com>
2025-03-03 23:56:17 +0100

    feat: Organize components and pages into a basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,78 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import Category from './pages/Category'
-import Checkout from './pages/Checkout'
-import Course from './pages/Course'
-import Mentor from './pages/Mentor'
-import OrderComplete from './pages/OrderComplete'
-import ShoppingCart from './pages/ShoppingCart'
-import StudentProfile from './pages/StudentProfile'
-import StudentLearning from './pages/StudentLearning'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+
+// Base Components
+import Auth from "./base/Auth";
+import Modal from "./base/Modal";
+import JsonPlaceholder from "./base/JsonPlaceholder";
+import Theme from "./base/Theme";
+
+// Pages Components
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Category from "./pages/Category";
+import Checkout from "./pages/Checkout";
+import Course from "./pages/Course";
+import Mentor from "./pages/Mentor";
+import OrderComplete from "./pages/OrderComplete";
+import ShoppingCart from "./pages/ShoppingCart";
+import StudentProfile from "./pages/StudentProfile";
+import StudentLearning from "./pages/StudentLearning";
+
+
+// Components
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+
+const AppLayout = () => {
+  const location = useLocation();
+  const AuthPage = location.pathname === "/login" || location.pathname === "/signup";
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        {/* Guest User Routes  */}
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* Authenticated User Routes  */}
+        <Route path="/category" element={<Category />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/course" element={<Course />} />
+        <Route path="/mentor" element={<Mentor />} />
+        <Route path="/order-complete" element={<OrderComplete />} />
+        <Route path="/shopping-cart" element={<ShoppingCart />} />
+        <Route path="/student-profile" element={<StudentProfile />} />
+        <Route path="/student-learning" element={<StudentLearning />} />
+      </Routes>
+      {!AuthPage && <Footer />}
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <Routes>
-          {/* Guest User Routes  */}
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          {/* Authenticated User Routes  */}
-          <Route path="/category" element={<Category />} />
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/course" element={<Course />} />
-          <Route path="/mentor" element={<Mentor />} />
-          <Route path="/order-complete" element={<OrderComplete />} />
-          <Route path="/shopping-cart" element={<ShoppingCart />} />
-          <Route path="/student-profile" element={<StudentProfile />} />
-          <Route path="/student-learning" element={<StudentLearning />} />
-        </Routes>
-      </Router>
+      <Auth>
+        <Modal>
+          <JsonPlaceholder>
+            <Theme>
+              <Router>
+                <AppLayout />
+              </Router>
+            </Theme>
+          </JsonPlaceholder>
+        </Modal>
+      </Auth>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
