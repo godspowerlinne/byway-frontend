5f33d91dbae844612177f754b9c46ff7d2d156aa
godspowerlinne <amriccygodspea@gmail.com>
2025-03-04 14:19:55 +0100

    feat: Implemented Navbar component with theme toggle, authentication check, and responsive design
    - Imported necessary icons and components for Navbar functionality
    - Added theme context to toggle between light and dark modes
    - Integrated authentication context to display appropriate user access components
    - Implemented responsive design with separate search bars for different screen sizes
    - Included menu toggle functionality for mobile navigation
    - Improved overall code readability and maintainability

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,73 @@
+import { AlignJustify, Moon, SunDim, X } from 'lucide-react';
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
+import { AuthUserAccessHub, GuestUserAccessHub, SearchBar, SearchBar2 } from '../utilities/Elements';
+import { useTheme } from '../base/Theme'
+import { useAuth } from '../base/Auth';
+
+const Navbar = () => {
+    // Use Theme Context 
+    const { isDarkMode, toggleTheme } = useTheme();
+
+    // Use Auth Context 
+    const { isAuthenticated } = useAuth();
+
+    // State for Menu open and close
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // Function to toggle menu open and close
+    const handleMenuToggle = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    return (
+        <header className='min-h-16 bg-white dark:bg-[#020617] text-[#334155] dark:text-white'>
+            <nav className='max-w-screen-2xl w-full mx-auto flex items-center justify-between px-4 md:px-8 xl:px-14 py-3 font-[500]'>
+                {/* Logo */}
+                <Link to="/" className='flex items-center gap-2 z-[100]'>
+                    <img className='h-10 w-auto object-cover' src='/public/byway.svg' alt='Logo' />
+                    <span className='text-[16px] dark:text-white'>Byway</span>
+                </Link>
+                <Link to="/category" className='hidden lg:flex dark:text-[#c19bff] hover:text-[#64748B] dark:hover:text-white transition-all md:text-[14px] lg:text-[16px]'>Categories</Link>
+                {/* Search bar  */}
+                <SearchBar />
+                <Link to="/mentor" className='hidden lg:flex dark:text-[#c19bff] hover:text-[#64748B] dark:hover:text-white transition-all text-[14px] xl:text-[16px]'>Teach on Byway</Link>
+                {/* Toggle Theme  */}
+                <button onClick={toggleTheme} className="hidden lg:flex p-1 rounded-full bg-gray-200 dark:bg-gray-800 hover:bg-gray-300 dark:hover:bg-gray-700 transition-all" >
+                    {isDarkMode ? (<SunDim className="text-yellow-400 scale-90 xl:scale-100" />) : (<Moon className="text-gray-400 scale-90 xl:scale-100" />)}
+                </button>
+                {/* User Access Hub  */}
+                <div className='hidden lg:flex scale-90 xl:scale-100'>
+                    {isAuthenticated ? (<AuthUserAccessHub />) : (<GuestUserAccessHub />)}
+                </div>
+                {/* Menu Icon */}
+                <button onClick={handleMenuToggle} className="lg:hidden p-2 z-[100]  dark:text-white" >
+                    {isMenuOpen ? (<X className="h-8 w-8" />) : (<AlignJustify className="h-8 w-8" />)}
+                </button>
+            </nav>
+            {/* Mobile nav */}
+            {isMenuOpen && (
+                <div className='fixed inset-0 z-50 lg:hidden'>
+                    <div className='absolute inset-0 bg-black/30 backdrop-blur-[2px] transition-opacity duration-300' onClick={handleMenuToggle} ></div>
+                    <div className='relative flex flex-col gap-4 items-center pt-20 p-8 max-w-[400px] w-full bg-white dark:bg-[#020617] h-[100%] min-h-[600px] shadow-xl'>
+                        <Link to="/category" className='rounded p-3 w-full bg-slate-100 dark:bg-slate-800 hover:bg-slate-50 dark:hover:bg-slate-700 hover:shadow-md transition-all' >Categories </Link>
+                        <Link to="/mentor" className='rounded p-3 w-full bg-slate-100 dark:bg-slate-800 hover:bg-slate-50 dark:hover:bg-slate-700 hover:shadow-md transition-all' >Teach on Byway </Link>
+                        {isAuthenticated ? (<AuthUserAccessHub />) : (<GuestUserAccessHub />)}
+                        <button onClick={toggleTheme} className="p-2 w-20 h-10 rounded-full relative flex items-center transition-all duration-300 ease-in-out border border-gray-200 dark:border-gray-700" >
+                            <div className="absolute w-8 h-8 rounded-full flex items-center justify-center transform transition-transform duration-300 ease-in-out shadow-md" style={{ backgroundColor: isDarkMode ? '#334155' : 'white', transform: isDarkMode ? 'translateX(32px)' : 'translateX(-5px)' }} >
+                                {isDarkMode ? (
+                                    <Moon className="w-5 h-5 text-yellow-400" />
+                                ) : (
+                                    <SunDim className="w-5 h-5 text-gray-400" />
+                                )}
+                            </div>
+                        </button>
+                        <SearchBar2 />
+                    </div>
+                </div>
+            )}
+        </header>
+    )
+}
+
+export default Navbar
