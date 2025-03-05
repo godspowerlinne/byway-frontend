33fff7b5f6811f17de8f5599f9cc2e4720b60e6c
godspowerlinne <amriccygodspea@gmail.com>
2025-03-05 16:43:28 +0100

    feat: Refine styling and responsiveness
    - Adjusted font sizes for improved readability across different screen sizes.
    - Removed pagination from the Swiper carousel for a cleaner look.
    - Updated button styling for better visual appeal.
    - Fine-tuned spacing and layout for a more polished appearance.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,15 +7,14 @@ import HeroImg4 from "/src/assets/byway_header_4.png";
 // Swiper imports
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import "swiper/css/pagination";
-import { Autoplay, Pagination } from "swiper/modules";
+import { Autoplay } from "swiper/modules";
 import { Button } from "../utilities/Elements";
 
 const HomeHero = () => {
     const HeroText = () => {
         return (
             <div className="max-w-[590px] flex flex-col gap-4">
-                <h1 className="text-[40px] font-bold capitalize leading-tight text-[#1E293B] dark:text-[#F1F5F9]">
+                <h1 className="text-[28px] md:text-[34px] lg:text-[40px] font-bold capitalize leading-tight text-[#1E293B] dark:text-[#F1F5F9]">
                     Unlock Your Potential with Byway
                 </h1>
                 <p className="text-[#334155] dark:text-[#CBD5E1]">
@@ -25,7 +24,7 @@ const HomeHero = () => {
                 </p>
                 <Button
                     BtnText="Start your instructor journey"
-                    BtnStyle="bg-[#3B82F6] dark:bg-[#410B65] text-white w-full lg:w-max justify-center lg:justify-start rounded-md mt-6 hover:bg-blue-700 dark:hover:bg-purple-700"
+                    BtnStyle="bg-[#3B82F6] dark:bg-[#410B65] text-white w-full lg:w-max justify-center lg:justify-start rounded-md mt-3 lg:mt-6 hover:bg-blue-700 dark:hover:bg-purple-700"
                 />
             </div>
         )
@@ -35,7 +34,7 @@ const HomeHero = () => {
         return (
             <div className="w-full flex flex-col lg:flex-row justify-between gap-8 items-center">
                 <HeroText />
-                <img src={heroImg.HeroImg} alt="Mentor Holding Class Notes" className="max-w-[500px]" />
+                <img src={heroImg.HeroImg} alt="Mentor Holding Class Notes" className="max-w-[500px] w-full" />
             </div>
         )
     }
@@ -43,14 +42,11 @@ const HomeHero = () => {
     return (
         <div className="px-4 md:px-8 xl:px-14 py-8 font-[500] max-w-screen-2xl w-full mx-auto  bg-white dark:bg-[#020617]">
             <Swiper
-                modules={[Autoplay, Pagination]}
-                spaceBetween={20}
+                modules={[Autoplay]}
+                spaceBetween={50}
                 slidesPerView={1}
                 loop={true}
                 speed={1000}
-                pagination={{
-                    clickable: true,
-                }}
                 autoplay={{ delay: 10000, disableOnInteraction: false }}
             >
                 <SwiperSlide>
